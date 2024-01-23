afb42334f8f9b3c48c9ff2f4980661ac3f78b0db
WilerMS <wilersoriano@gmail.com>
2024-01-23 16:24:56 +0100

    using new components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
 import { type FormEvent, useState, useRef, useEffect } from 'react'
 import cn from 'classnames'
 
-import AnswerInput from '@components/AnswerInput'
+import GameInput from '@components/Game/GameInput'
 import RandomDivs from '@components/RandomDivs'
 import Score from '@components/Score'
 import irregularVerbs from '@constants/irregular-verbs'
 import { disorderArray } from '@utils/index'
 import { useLocalStorage } from '@hooks/useLocalStorage'
-import Check from '@components/Check'
+import GameContainer from '@components/Game/GameContainer'
+import GameButton from '@components/Game/GameButton'
+import GamePrompt from '@components/Game/GamePrompt'
 
 const App = () => {
   const [verbs, setVerbs] = useState(() => disorderArray(irregularVerbs))
@@ -111,13 +113,12 @@ const App = () => {
 
   return (
     <div className="bg-gradient w-screen min-h-screen center">
-      <div className="relative bg-glass min-w-[400px] max-w-[750px] max-h-[625px] rounded-2xl z-10 center overflow-hidden p-2 transition-all duration-300">
-
+      <GameContainer>
         <form
           className={cn(
             'w-full h-full p-10 z-10',
             changing ? 'opacity-0' : '',
-            'relative !bg-white !bg-opacity-20 grid grid-rows-[2fr_3fr] rounded-xl overflow-hidden transition-all duration-500'
+            'relative grid grid-rows-[2fr_3fr] transition-all duration-500'
           )}
           onSubmit={handleClickNext}
         >
@@ -126,58 +127,52 @@ const App = () => {
             <Score score={score} highScore={highScore} />
           </div>
 
-          <div id="word" className='center'>
-            <h2 className='text-[#ffffffec] text-shadow-lg font-extrabold text-7xl uppercase mt-4 break-all text-center transition-all duration-300'>
-              {currentWord}
-            </h2>
-          </div>
+          <GamePrompt title={currentWord} />
 
           {/* ANSWER APTIONS */}
           <div className="options flex flex-col gap-1">
-            <AnswerInput
-              inputRef={infinitiveRef}
-              name='infinitive'
-              success={infinitiveSucces}
-              value={inputData.infinitive}
-              onChange={handleWriteAnswers}
-              placeholder='Write the verb in infinitive'
-            />
-            <AnswerInput
-              inputRef={pastRef}
-              name='past'
-              success={pastSucces}
-              value={inputData.past}
-              onChange={handleWriteAnswers}
-              placeholder='Write the verb in past'
-            />
-            <AnswerInput
-              inputRef={participleRef}
-              name='participle'
-              success={participleSucces}
-              value={inputData.participle}
-              onChange={handleWriteAnswers}
-              placeholder='Write the verb in participle'
-            />
+            <div className="h-[90px]">
+              <GameInput
+                inputRef={infinitiveRef}
+                name='infinitive'
+                success={infinitiveSucces}
+                value={inputData.infinitive}
+                onChange={handleWriteAnswers}
+                placeholder='Write the verb in infinitive'
+              />
+            </div>
+            <div className="h-[90px]">
+              <GameInput
+                inputRef={pastRef}
+                name='past'
+                success={pastSucces}
+                value={inputData.past}
+                onChange={handleWriteAnswers}
+                placeholder='Write the verb in past'
+              />
+            </div>
+            <div className="h-[90px]">
+              <GameInput
+                inputRef={participleRef}
+                name='participle'
+                success={participleSucces}
+                value={inputData.participle}
+                onChange={handleWriteAnswers}
+                placeholder='Write the verb in participle'
+              />
+            </div>
           </div>
 
           {/* ACTION BUTTONS */}
           <div className="action">
             <div className="w-full h-full flex justify-between items-center gap-5">
-              <button
-                type="button"
-                onClick={handleClickSkip}
-                className="w-full text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br shadow-lg shadow-red-500/50 font-medium rounded-lg text-sm px-5 py-3 text-center transition-all duration-300 hover:scale-[1.02]"
-              >
+              <GameButton variant='danger' onClick={handleClickSkip}>
                 <span>SKIP</span>
-              </button>
-              <button
-                type="submit"
-                className="w-full text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br shadow-lg shadow-green-500/50 font-medium rounded-lg text-sm px-5 py-3 text-center transition-all duration-300 hover:scale-[1.02]"
-              >
+              </GameButton>
+              <GameButton type="submit" variant='success'>
                 <span>NEXT</span>
-              </button>
+              </GameButton>
             </div>
-
           </div>
         </form>
 
@@ -190,16 +185,17 @@ const App = () => {
           )}
         >
           <div>
-          {changing &&
-            <div>
-              <h3 className='text-[#ffffffec] text-shadow-lg font-extrabold text-4xl uppercase mt-4 break-all text-center transition-all duration-300'>{verbs[index].infinitive}</h3>
-              <h3 className='text-[#ffffffec] text-shadow-lg font-extrabold text-4xl uppercase mt-4 break-all text-center transition-all duration-300'>{verbs[index].past}</h3>
-              <h3 className='text-[#ffffffec] text-shadow-lg font-extrabold text-4xl uppercase mt-4 break-all text-center transition-all duration-300'>{verbs[index].participle}</h3>
-            </div>}
+            {changing &&
+              <div>
+                <h3 className='text-[#ffffffec] text-shadow-lg font-extrabold text-4xl uppercase mt-4 break-all text-center transition-all duration-300'>{verbs[index].infinitive}</h3>
+                <h3 className='text-[#ffffffec] text-shadow-lg font-extrabold text-4xl uppercase mt-4 break-all text-center transition-all duration-300'>{verbs[index].past}</h3>
+                <h3 className='text-[#ffffffec] text-shadow-lg font-extrabold text-4xl uppercase mt-4 break-all text-center transition-all duration-300'>{verbs[index].participle}</h3>
+              </div>
+            }
           </div>
 
         </div>
-      </div>
+      </GameContainer>
 
       <RandomDivs
         className='fixed w-full h-full'
