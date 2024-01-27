e251f88272b92ddb6d01db5c300f864e6cbf58e4
WilerMS <wilersoriano@gmail.com>
2024-01-27 15:00:35 +0100

    import queryclient

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,15 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const root = document.getElementById('root') as HTMLElement
+const queryClient = new QueryClient()
 
 const Main = () => {
   return (
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   )
 }
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <Main />
-  </React.StrictMode>
-)
+ReactDOM
+  .createRoot(root)
+  .render(
+    <React.StrictMode>
+      <Main />
+    </React.StrictMode>
+  )
